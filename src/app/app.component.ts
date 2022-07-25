b7f72ad75460f96066f12b26d1f421045b56461b
Sanchklyc <halitenes.kalayci@gmail.com>
2022-07-25 18:45:24 +0300

    '25.07.2022'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'etiya-angular';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.counter += 5;
+  }
+  counter: number = 0;
+
+  increase(count: number = 1) {
+    this.counter += count;
+  }
+
+  get Counter() {
+    return (
+      this.counter.toString() + (this.counter < 5 ? ' Çok az' : ' Çok fazla')
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProductListComponent, AddProductComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
