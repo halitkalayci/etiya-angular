3942a2b3b12e3c9fb2d804ea4c64ead5e91ab163
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-07-29 19:16:15 +0300

    29.07.2022 post form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { Customer } from 'src/app/models/customer';
+import { CustomersService } from 'src/app/services/customers/customers.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registerForm!: FormGroup;
+  registerForm!: FormGroup; // state
 
   // companyName = new FormControl('', Validators.required);
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private customerService: CustomersService) {}
 
   ngOnInit(): void {
     this.createRegisterForm();
@@ -22,7 +25,16 @@ export class RegisterComponent implements OnInit {
       companyName: [
         '', // default değer
         [Validators.required, Validators.minLength(2)] // doğrulama
-      ]
+      ],
+      contactName: ['', Validators.required],
+      contactTitle: ['', Validators.required],
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      region: ['', Validators.required],
+      postalCode: ['', Validators.required],
+      country: ['', Validators.required],
+      phone: ['', Validators.required],
+      customerKey: ['', Validators.required]
     });
     // new FormGroup({
     //   companyName: this.companyName,
@@ -30,12 +42,19 @@ export class RegisterComponent implements OnInit {
   }
 
   register(): void {
-    if (!this.registerForm.valid) {
+    if (this.registerForm.invalid) {
       // geçersiz
       console.warn('Gerekli alanları doldurunuz.');
       return;
     }
 
-    console.log(this.registerForm.value);
+    const customer: Customer = {
+      ...this.registerForm.value,
+      city: this.registerForm.value.city.toUpperCase()
+    };
+
+    this.customerService.add(customer).subscribe(response => {
+      console.info(response);
+    });
   }
 }
---
+++ src/app/services/customers/customers.service.ts
@@ -0,0 +1,18 @@
+import { Customer } from 'src/app/models/customer';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomersService {
+  apiControllerUrl: string = `${environment.apiUrl}/customers`; // property
+
+  constructor(private httpClient: HttpClient) {}
+
+  add(customer: Customer): Observable<Customer> {
+    return this.httpClient.post<Customer>(this.apiControllerUrl, customer);
+  }
+}
