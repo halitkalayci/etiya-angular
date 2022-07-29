6e5e88cfe88e40b421058b9f4d4db82a64baae6f
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-07-29 18:22:05 +0300

    29.07.2022 reactive form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,18 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  companyName = new FormControl('', Validators.required);
-  constructor() {}
+  registerForm!: FormGroup;
 
-  ngOnInit(): void {}
+  // companyName = new FormControl('', Validators.required);
 
-  register() {
-    console.log(this.companyName);
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm(): void {
+    this.registerForm = this.formBuilder.group({
+      companyName: [
+        '', // default değer
+        [Validators.required, Validators.minLength(2)] // doğrulama
+      ]
+    });
+    // new FormGroup({
+    //   companyName: this.companyName,
+    // });
+  }
+
+  register(): void {
+    if (!this.registerForm.valid) {
+      // geçersiz
+      console.warn('Gerekli alanları doldurunuz.');
+      return;
+    }
+
+    console.log(this.registerForm.value);
   }
 }
