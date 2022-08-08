48c19ed122ad62e6edafd6c985a24d96f19ad145
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-08-08 09:58:09 +0300

    08.08.2022 add error handling interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,8 @@
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
+import { ErrorHandlingInterceptor } from './interceptors/error-handling/error-handling.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { IfNotDirective } from './directives/ifNot/if-not.directive';
 import { NgModule } from '@angular/core';
 import { StorageModule } from './storage/storage.module';
@@ -8,6 +10,7 @@ import { StorageModule } from './storage/storage.module';
 @NgModule({
   declarations: [IfNotDirective],
   imports: [CommonModule, CoreRoutingModule, AuthModule, StorageModule],
-  exports: [IfNotDirective]
+  exports: [IfNotDirective],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: ErrorHandlingInterceptor, multi: true }]
 })
 export class CoreModule {}
